46cadad69f8ad7b60053f40218172ca1881f6bbd
Max-GO-IT <64358225+Max-GO-IT@users.noreply.github.com>
2024-06-18 20:17:38 +0000

    Deploying to gh-pages from @ Max-GO-IT/goit-js-hw-11@d29f7adc4aa2e5773d24daee07d8caa03ac13445 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,22 +1,22 @@
-import{i as a,S as l}from"./assets/vendor-0fc460d7.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const o of t)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function e(t){const o={};return t.integrity&&(o.integrity=t.integrity),t.referrerPolicy&&(o.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?o.credentials="include":t.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function s(t){if(t.ep)return;t.ep=!0;const o=e(t);fetch(t.href,o)}})();function u(i){const r=`https://pixabay.com/api/?key=44433569-eda6d7623baf54ab2611b04f8&q=${i}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(r).then(e=>{if(!e.ok)throw new Error("Не вдалося отримати дані від Pixabay API");return e.json()}).then(e=>{if(e.hits.length===0)throw a.error({title:"Помилка!",message:"Нічого не знайдено за вашим запитом"});return e.hits}).catch(e=>{throw a.error({title:"Помилка!",message:e.message}),e})}function f(i){const r=document.querySelector("#gallery"),e=i.map(s=>`
+import{i as a,S as d}from"./assets/vendor-0fc460d7.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))n(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&n(i)}).observe(document,{childList:!0,subtree:!0});function e(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function n(t){if(t.ep)return;t.ep=!0;const r=e(t);fetch(t.href,r)}})();function u(s){const o=`https://pixabay.com/api/?key=44433569-eda6d7623baf54ab2611b04f8&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`;return fetch(o).then(e=>{if(!e.ok)throw new Error("Не вдалося отримати дані від Pixabay API");return e.json()}).then(e=>{if(e.hits.length===0)throw a.error({title:"Помилка!",message:"Нічого не знайдено за вашим запитом"});return e.hits}).catch(e=>{throw a.error({title:"Помилка!",message:e.message}),e})}function f(s){const o=document.querySelector("#gallery"),e=s.map(n=>`
       <div class="photo-card">
-        <a class="gallery-link" href="${s.largeImageURL}">
-          <img src="${s.webformatURL}" alt="${s.tags}" loading="lazy" />
+        <a class="gallery-link" href="${n.largeImageURL}">
+          <img src="${n.webformatURL}" alt="${n.tags}" loading="lazy" />
         </a>
         <div class="info">
           <p class="info-item">
-            <b>${s.likes}</b> likes
+            <b>${n.likes}</b> likes
           </p>
           <p class="info-item">
-            <b>${s.views}</b> views
+            <b>${n.views}</b> views
           </p>
           <p class="info-item">
-            <b>${s.comments}</b> views
+            <b>${n.comments}</b> comments
           </p>
           <p class="info-item">
-            <b>${s.downloads}</b> views
+            <b>${n.downloads}</b> downloads
           </p>
         </div>
       </div>
-    `).join("");r.insertAdjacentHTML("beforeend",e)}const d=document.querySelector("#search-form"),c=document.querySelector("#gallery");document.querySelector("#loading");d.addEventListener("submit",i=>{i.preventDefault();const r=document.querySelector("#search-input").value.trim();r!==""&&(c.innerHTML="",u(r).then(e=>{f(e)}).catch(e=>{}))});new l(".gallery-link",{captionDelay:250,captionPosition:"bottom",captionType:"attr",captionsData:"alt"});c.addEventListener("click",i=>{if(i.target.classList.contains("gallery-image")){const r=i.target.dataset.source;console.log("Ссылка на большое изображение:",r)}});
+    `).join("");o.insertAdjacentHTML("beforeend",e)}const p=document.querySelector("#search-form"),c=document.querySelector("#gallery"),l=document.querySelector("#loading");p.addEventListener("submit",s=>{s.preventDefault();const o=document.querySelector("#search-input").value.trim();o!==""&&(c.innerHTML="",document.getElementById("loading").style.display="none",l.style.display="none",u(o).then(e=>{f(e),l.style.display="none"}).catch(e=>{l.style.display="flex"}))});c.addEventListener("click",s=>{s.preventDefault(),console.log(s.currentTarget),new d(".gallery-link",{captionDelay:250,captionPosition:"bottom",captionType:"attr",captionsData:"alt"})});
 //# sourceMappingURL=commonHelpers.js.map
